07b5e4e5c88673d68b4c263faf4e7502d5a0892a
PaterDeTenebis <onleerave@gmail.com>
2022-09-21 23:14:22 +0300

    Migration done

---
+++ fairtoken/pages/_app.js
@@ -0,0 +1,32 @@
+import '../styles/globals.css';
+import '../styles/landing.css';
+import Head from 'next/head';
+import Script from 'next/script';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        // Responsive meta tag
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
+          rel="stylesheet"
+          integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT"
+          crossorigin="anonymous"
+        />
+      </Head>
+      <Script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8"
+        crossorigin="anonymous"
+      />
+      <Script src="https://kit.fontawesome.com/e53c3054f5.js" crossorigin="anonymous" />
+      
+
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
---
+++ fairtoken/pages/index.js
@@ -0,0 +1,186 @@
+import $ from 'jquery';
+export default function Home() {
+  return (
+    <div className="landing_main">
+      <section className="landing_promo">
+        <div className="container">
+          <div className="landing_menu">
+            <a href="#" className="landing_menuItem landing_textCommon">
+              fairpaper
+            </a>
+            <a href="#landing_tokenomics" className="landing_menuItem landing_textCommon">
+              tokenomics
+            </a>
+            <a href="#landing_lottery" className="landing_menuItem landing_textCommon">
+              lottery
+            </a>
+            <a href="#landing_social" className="landing_menuItem landing_textCommon">
+              social
+            </a>
+          </div>
+          <div className="landing_promoMain">
+            <div className="landing_sectionHeader">
+              welcome to <br />
+              fairtoken family
+            </div>
+            <div className="landing_textXl">worlds top-1 defi community</div>
+            <div className="landing_promoMain_contract">
+              <div className="d-flex">
+                <img src="img/landing_logo.png" alt="logo" />
+                <div className="d-block">
+                  <span className="landing_textXXL">fair</span>
+                  <br />
+                  <span className="landing_textMd">token</span>
+                </div>
+              </div>
+              <div className="landing_textCommon">Contract address: 0xjeoeodnbdksodohdh</div>
+              <div className="landing_watch">
+                <button className="landing_watchBtn">
+                  <i className="fa-solid fa-play"></i>
+                </button>
+                <div className="landing_textCommon">watch video</div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="landing_tokenomics" id="landing_tokenomics">
+        <div className="container">
+          <div className="landing_sectionHeader">tokenomics</div>
+          <div className="landing_tokenomicsMain">
+            <div className="d-flex">
+              <img src="img/landing_tokenomics.png" alt="" className="landing_tokenomicsPie" />
+              <div className="landing_textCommon">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab perspiciatis dicta optio
+                error earum corporis ad eos, magnam dolore facere consectetur, sequi, quisquam sit
+                quos maxime? Architecto sint quo quas deleniti recusandae velit rerum sed
+                consequuntur optio autem nihil veniam quia iure, esse ea voluptatibus, adipisci
+                veritatis, nostrum vitae. Deleniti?
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="landing_lottery" id="landing_lottery">
+        <div className="container">
+          <div className="landing_sectionHeader">lottery</div>
+          <div className="landing_lotteryMain">
+            <div className="landing_lotteryMain_item">
+              <div className="landing_sectionHeader">12</div>
+              <div className="landing_textMinor">hours</div>
+            </div>
+            <div className="landing_lotteryMain_item">
+              <div className="landing_sectionHeader">12</div>
+              <div className="landing_textMinor">minutes</div>
+            </div>
+            <div className="landing_lotteryMain_item">
+              <div className="landing_sectionHeader">12</div>
+              <div className="landing_textMinor">seconds</div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <div className="landing_bgWraper">
+        <section className="landing_winners" id="landing_winners">
+          <div className="container">
+            <div className="landing_sectionHeader">winners</div>
+            <div className="landing_winnersMain">
+              <div className="landing_winnersMain_line">
+                <div className="landing_winnersMain_lineItem">
+                  <div className="landing_textCommon landing_textGreen">address</div>
+                </div>
+                <div className="landing_winnersMain_lineItem">
+                  <div className="landing_textCommon landing_textGreen">transaction</div>
+                </div>
+                <div className="landing_winnersMain_lineItem">
+                  <div className="landing_textCommon landing_textGreen">prize</div>
+                </div>
+              </div>
+              <div className="landing_winnersMain_line">
+                <div className="landing_winnersMain_lineItem">
+                  <div className="landing_textCommon">0xjeoeodnb dksodohdh</div>
+                </div>
+                <div className="landing_winnersMain_lineItem">
+                  <div className="landing_textCommon">18.09.2022</div>
+                </div>
+                <div className="landing_winnersMain_lineItem">
+                  <div className="landing_textCommon">1000000 frt</div>
+                </div>
+              </div>
+              <div className="landing_winnersMain_line">
+                <div className="landing_winnersMain_lineItem">
+                  <div className="landing_textCommon">0xjeoeodnb dksodohdh</div>
+                </div>
+                <div className="landing_winnersMain_lineItem">
+                  <div className="landing_textCommon">18.09.2022</div>
+                </div>
+                <div className="landing_winnersMain_lineItem">
+                  <div className="landing_textCommon">1000000 frt</div>
+                </div>
+              </div>
+              <div className="landing_winnersMain_line">
+                <div className="landing_winnersMain_lineItem">
+                  <div className="landing_textCommon">0xjeoeodnb dksodohdh</div>
+                </div>
+                <div className="landing_winnersMain_lineItem">
+                  <div className="landing_textCommon">18.09.2022</div>
+                </div>
+                <div className="landing_winnersMain_lineItem">
+                  <div className="landing_textCommon">1000000 frt</div>
+                </div>
+              </div>
+              <div className="landing_winnersMain_line">
+                <div className="landing_winnersMain_lineItem">
+                  <div className="landing_textCommon">0xjeoeodnb dksodohdh</div>
+                </div>
+                <div className="landing_winnersMain_lineItem">
+                  <div className="landing_textCommon">18.09.2022</div>
+                </div>
+                <div className="landing_winnersMain_lineItem">
+                  <div className="landing_textCommon">1000000 frt</div>
+                </div>
+              </div>
+            </div>
+            <div className="landing_winnersMore">
+              <a
+                href="#winners"
+                className="landing_textCommon landing_textGreen"
+                id="landing_winnersMore"
+                onClick={(e) => {
+                  e.preventDefault();
+                  $('.landing_winnersMain').toggleClass('landing_hA');
+                }}>
+                watch history
+              </a>
+            </div>
+          </div>
+        </section>
+        <section className="landing_social" id="landing_social">
+          <div className="container">
+            <div className="landing_sectionHeader">social media</div>
+            <div className="landing_textXl">
+              Subscribe to our official resources and stay tuned!
+            </div>
+            <div className="landing_socialMain">
+              <button className="landing_watchBtn">
+                <i className="fa-brands fa-telegram"></i>
+              </button>
+              <button className="landing_watchBtn">
+                <i className="fa-brands fa-telegram"></i>
+              </button>
+              <button className="landing_watchBtn">
+                <i className="fa-brands fa-discord"></i>
+              </button>
+              <button className="landing_watchBtn">
+                <i className="fa-brands fa-twitter"></i>
+              </button>
+              <button className="landing_watchBtn">
+                <i className="fa-brands fa-youtube"></i>
+              </button>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+}
