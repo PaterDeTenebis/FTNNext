d096ed22900f5fa20da672741d398ad367314900
PaterDeTenebis <onleerave@gmail.com>
2022-10-03 19:45:01 +0300

    classNames refactor

--- fairtoken/pages/cabinet.js
+++ fairtoken/pages/cabinet.js
@@ -1,72 +1,74 @@
 import $ from 'jquery';
 export default function Home() {
   return (
-    <div class="cabinetWrapper">
-      <div class="">
-        <div class="cabinet_nav">
-          <a href="#" class="cabinet_logo">
-            <img src="img/landing_logo.png" alt="logo" class="cabinet_logoImg" />
-            <div class="cabinet_logoText">
+    <div className="cabinetWrapper">
+      <div className="">
+        <div className="cabinet_nav">
+          <a href="#" className="cabinet_logo">
+            <img src="img/landing_logo.png" alt="logo" className="cabinet_logoImg" />
+            <div className="cabinet_logoText">
               fair
-              <span class="cabinet_logoTextSm">token</span>
+              <span className="cabinet_logoTextSm">token</span>
             </div>
           </a>
-          <div class="cabinet_navBtnHolder">
-            <button class="cabinet_connect">Connect Wallet</button>
-            <button class="cabinet_Close">x</button>
+          <div className="cabinet_navBtnHolder">
+            <button className="cabinet_connect">Connect Wallet</button>
+            <button className="cabinet_Close">x</button>
           </div>
         </div>
-        <div class="cabinet_marketHolder">
-          <div class="cabinet_textMd cabinet_textLight">1 FTN = 12 USD</div>
-          <button class="cabinet_buy">Buy Token</button>
-          <button class="cabinet_sell">Sell Token</button>
+        <div className="cabinet_marketHolder">
+          <div className="cabinet_textMd cabinet_textLight">1 FTN = 12 USD</div>
+          <button className="cabinet_buy">Buy Token</button>
+          <button className="cabinet_sell">Sell Token</button>
         </div>
-        <div class="cabinet_topLine">
-          <div class="cabinet_itemBordered">
-            <div class="cabinet_itemBordered_side cabinet_textSecondary">BNB Balance</div>
-            <div class="cabinet_itemBordered_side cabinet_textLg">
-              12344<span class="cabinet_lableG">BNB</span>
+        <div className="cabinet_topLine">
+          <div className="cabinet_itemBordered">
+            <div className="cabinet_itemBordered_side cabinet_textSecondary">BNB Balance</div>
+            <div className="cabinet_itemBordered_side cabinet_textLg">
+              12344<span className="cabinet_lableG">BNB</span>
             </div>
-            <div class="cabinet_itemBordered_side cabinet_textSecondary">BNB Earned</div>
-            <div class="cabinet_itemBordered_side cabinet_textLg">
-              12344<span class="cabinet_lableG">BNB</span>
-            </div>
-          </div>
-          <div class="cabinet_itemBordered">
-            <div class="cabinet_itemBordered_side cabinet_textSecondary">FTN Balance</div>
-            <div class="cabinet_itemBordered_side cabinet_textLg">
-              12344<span class="cabinet_lableG">FTN</span>
-            </div>
-            <div class="cabinet_itemBordered_side cabinet_textSecondary">FTN Earned</div>
-            <div class="cabinet_itemBordered_side cabinet_textLg">
-              12344<span class="cabinet_lableG">FTN</span>
+            <div className="cabinet_itemBordered_side cabinet_textSecondary">BNB Earned</div>
+            <div className="cabinet_itemBordered_side cabinet_textLg">
+              12344<span className="cabinet_lableG">BNB</span>
             </div>
           </div>
-          <div class="cabinet_itemBordered">
-            <div class="cabinet_itemBordered_side cabinet_textSecondary">Refferals First line</div>
-            <div class="cabinet_itemBordered_side cabinet_textLg">123</div>
-            <div class="cabinet_itemBordered_side cabinet_textSecondary">
+          <div className="cabinet_itemBordered">
+            <div className="cabinet_itemBordered_side cabinet_textSecondary">FTN Balance</div>
+            <div className="cabinet_itemBordered_side cabinet_textLg">
+              12344<span className="cabinet_lableG">FTN</span>
+            </div>
+            <div className="cabinet_itemBordered_side cabinet_textSecondary">FTN Earned</div>
+            <div className="cabinet_itemBordered_side cabinet_textLg">
+              12344<span className="cabinet_lableG">FTN</span>
+            </div>
+          </div>
+          <div className="cabinet_itemBordered">
+            <div className="cabinet_itemBordered_side cabinet_textSecondary">
+              Refferals First line
+            </div>
+            <div className="cabinet_itemBordered_side cabinet_textLg">123</div>
+            <div className="cabinet_itemBordered_side cabinet_textSecondary">
               Holder recieve in Token / BNB
             </div>
-            <div class="cabinet_itemBordered_side cabinet_textLg">
-              123<span class="cabinet_lableG">FTN</span> / 1212
-              <span class="cabinet_lableG">BNB</span>
+            <div className="cabinet_itemBordered_side cabinet_textLg">
+              123<span className="cabinet_lableG">FTN</span> / 1212
+              <span className="cabinet_lableG">BNB</span>
             </div>
-            <div class="cabinet_itemBordered_side cabinet_textSecondary">
+            <div className="cabinet_itemBordered_side cabinet_textSecondary">
               Total recive in Lottery
             </div>
-            <div class="cabinet_itemBordered_side cabinet_textLg">
-              123<span class="cabinet_lableG">FTN</span>
+            <div className="cabinet_itemBordered_side cabinet_textLg">
+              123<span className="cabinet_lableG">FTN</span>
             </div>
           </div>
         </div>
-        <div class="cabinet_botLine">
-          <div class="cabinet_itemBordered">
-            <div class="cabinet_itemBordered_side cabinet_textSecondary cabinet_tableBtnHolder">
+        <div className="cabinet_botLine">
+          <div className="cabinet_itemBordered">
+            <div className="cabinet_itemBordered_side cabinet_textSecondary cabinet_tableBtnHolder">
               Total Locked: 10 000 BNB
-              <button class="cabinet_buy">Unlock</button>
+              <button className="cabinet_buy">Unlock</button>
             </div>
-            <table class="table cabinet_textLight">
+            <table className="table cabinet_textLight">
               <thead>
                 <tr>
                   <th scope="col">#</th>
@@ -87,13 +89,12 @@ export default function Home() {
               </tbody>
             </table>
           </div>
-          <div class="cabinet_itemBordered">
-            <div class="cabinet_itemBordered_side cabinet_textSecondary cabinet_tableBtnHolder">
+          <div className="cabinet_itemBordered">
+            <div className="cabinet_itemBordered_side cabinet_textSecondary cabinet_tableBtnHolder">
               Total Locked: 10 000 BNB
-              <button class="cabinet_buy">Harvest</button>
-
+              <button className="cabinet_buy">Harvest</button>
             </div>
-            <table class="table cabinet_textLight">
+            <table className="table cabinet_textLight">
               <thead>
                 <tr>
                   <th scope="col">#</th>
@@ -114,11 +115,11 @@ export default function Home() {
               </tbody>
             </table>
           </div>
-          <div class="cabinet_itemBordered">
-            <div class="cabinet_itemBordered_side cabinet_textSecondary">
+          <div className="cabinet_itemBordered">
+            <div className="cabinet_itemBordered_side cabinet_textSecondary">
               Total Locked: 10 000 BNB
             </div>
-            <table class="table cabinet_textLight">
+            <table className="table cabinet_textLight">
               <thead>
                 <tr>
                   <th scope="col">#</th>
